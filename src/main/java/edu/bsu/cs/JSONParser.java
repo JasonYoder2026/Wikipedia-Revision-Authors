47fbb717cae6e8e5024c47dbca8aa4470beec9a4
Jason <141192101+jyod27@users.noreply.github.com>
2024-09-18 09:12:37 -0400

    Improves JSONParser JSONParser returns first user and timestamp

--- src/main/java/edu/bsu/cs/JSONParser.java
+++ src/main/java/edu/bsu/cs/JSONParser.java
@@ -1,14 +1,19 @@
 package edu.bsu.cs;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import com.jayway.jsonpath.JsonPath;
 import net.minidev.json.JSONArray;
 
 public class JSONParser {
 
-    public String parse(InputStream testDataStream) throws IOException {
+    public String parseTimestampData(File testDataStream) throws IOException {
         JSONArray result = (JSONArray)JsonPath.read(testDataStream, "$..timestamp");
+        return result.getFirst().toString();
+    }
+
+    public String parseUsernameData(File testData) throws IOException {
+        JSONArray result = (JSONArray) JsonPath.read(testData, "$..user");
         return result.getFirst().toString();
     }
 }
--- src/test/java/edu/bsu/cs/JSONParserTest.java
+++ src/test/java/edu/bsu/cs/JSONParserTest.java
@@ -3,17 +3,25 @@ package edu.bsu.cs;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class JSONParserTest {
 
     @Test
     public void testParser() throws IOException {
-        JSONParser parser = new JSONParser();
-        InputStream testDataStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("JsonData.json");
-        String timeStamp = parser.parse(testDataStream);
+        JSONParser testParser = new JSONParser();
+        File testData = new File("JsonData.json");
+        String timeStamp = testParser.parseTimestampData(testData);
         Assertions.assertEquals("2024-09-15T22:53:03Z", timeStamp);
+    }
+
+    @Test
+    public void testUserParser() throws IOException {
+        JSONParser testParser = new JSONParser();
+        File testData = new File("JsonData.json");
+        String user = testParser.parseUsernameData(testData);
+        Assertions.assertEquals("Aaw1989", user);
     }
 
 }
