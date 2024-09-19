57ce84a3e949ac48b6bfb44a613d739b301c1f6f
Jason <141192101+jyod27@users.noreply.github.com>
2024-09-19 18:29:08 -0400

    Adds resources folder and corrects test methods file path accordingly

--- src/test/java/edu/bsu/cs/JSONHandlerTest.java
+++ src/test/java/edu/bsu/cs/JSONHandlerTest.java
@@ -2,7 +2,6 @@ package edu.bsu.cs;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -11,20 +10,51 @@ public class JSONHandlerTest {
     @Test
     public void testParseAndPrintData() throws IOException {
         JSONHandler testParser = new JSONHandler();
-        InputStream testStream = new FileInputStream("JsonData.json");
+        InputStream testStream = getJsonDataStream();
         String result = testParser.printData(testStream);
-        Assertions.assertEquals("Redirected to Frank Zappa.\n" +
-                "There are only 3 revisions.\n" +
-                "2024-09-15T22:53:03Z  Aaw1989\n2024-09-15T20:04:58Z  2A00:23C5:E212:D01:9D11:46F1:12D5:47DB\n" +
-                "2024-09-15T19:47:56Z  Aaw1989\n", result);
+        Assertions.assertEquals("""
+                Redirected to Frank Zappa.
+                There are only 3 revisions.
+                2024-09-15T22:53:03Z  Aaw1989
+                2024-09-15T20:04:58Z  2A00:23C5:E212:D01:9D11:46F1:12D5:47DB
+                2024-09-15T19:47:56Z  Aaw1989
+                """, result);
+    }
+
+    public InputStream getJsonDataStream() {
+        try {
+            InputStream testStream = JSONHandlerTest.class.getClassLoader().getResourceAsStream("JsonData.json");
+            if(testStream == null) {
+                throw new IllegalArgumentException("File not found!");
+            }
+            return testStream;
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     @Test
     public void testMissingArticle() throws IOException {
         JSONHandler testHandler = new JSONHandler();
-        InputStream testStream = new FileInputStream("Missing.json");
+        InputStream testStream = getMissingArticleStream();
         String result = testHandler.printData(testStream);
         Assertions.assertEquals("System error: No Wikipedia page with that title.\n", result);
+    }
+
+    public InputStream getMissingArticleStream() {
+        try {
+            InputStream testStream = JSONHandlerTest.class.getClassLoader().getResourceAsStream("Missing.json");
+            if(testStream == null) {
+                throw new IllegalArgumentException("File not found!");
+            }
+            return testStream;
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
     @Test
@@ -33,4 +63,6 @@ public class JSONHandlerTest {
         String output = testParser.formatTimeAndUsername("2024-09-15T22:53:03Z", "Aaw1989");
         Assertions.assertEquals("2024-09-15T22:53:03Z  Aaw1989\n", output);
     }
+
+
 }
