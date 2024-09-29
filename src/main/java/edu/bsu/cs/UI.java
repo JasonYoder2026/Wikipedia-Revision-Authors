0f4088cd7a7f752ef5bee2c2c1f58850cd0ba77b
Jason <141192101+jyod27@users.noreply.github.com>
2024-09-29 18:31:40 -0400

    Enhances GUI using gridpane

--- src/main/java/edu/bsu/cs/UI.java
+++ src/main/java/edu/bsu/cs/UI.java
@@ -1,12 +1,14 @@
 package edu.bsu.cs;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class UI extends Application {
@@ -15,12 +17,16 @@ public class UI extends Application {
     }
 
     private final Button searchButton = new Button("Search");
-    private final TextField searchBox = new TextField("Enter the name of an article");
+    private final Label searchLabel = new Label("Enter the name of an article: ");
+    private final TextField searchBox = new TextField();
     private final TextArea resultsBox = new TextArea();
 
 
     @Override
     public void start(Stage mainStage) {
+        mainStage.setWidth(600);
+        mainStage.setHeight(550);
+        mainStage.setResizable(false);
         resultsBox.setEditable(false);
         configureStage(mainStage);
         configureSearchButton();
@@ -29,19 +35,23 @@ public class UI extends Application {
     private void configureStage(Stage stage) {
         stage.setTitle("WikiMedia Revisions App");
         stage.setScene(new Scene(createRoot()));
-        stage.setMaxHeight(650.0);
-        stage.setMaxWidth(600.0);
         stage.show();
     }
 
-    private Pane createRoot() {
-        VBox root = new VBox();
-        root.getChildren().addAll(
-                searchBox,
-                searchButton,
-                resultsBox
-            );
-        return root;
+    private GridPane createRoot() {
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(20));
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+
+        gridPane.setAlignment(Pos.CENTER);
+        resultsBox.setPrefHeight(350);
+        gridPane.add(searchLabel, 0, 0);
+        gridPane.add(searchBox, 1, 0);
+        gridPane.add(searchButton, 1, 1);
+        gridPane.add(resultsBox, 0, 2, 2, 1);
+
+        return gridPane;
     }
 
     private void configureSearchButton() {
